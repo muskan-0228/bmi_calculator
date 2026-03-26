921d4f1df36ede4dcf5cb2eed0d41b293474a013
Muskan Bharti <muskanbharti9113@gmail.com>
2026-03-26 21:55:54 +0530

    spelling mistake

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
 import "./App.css";
 import "./index.css";
-import React, {useState} from "react";
+import React, {useState}  from "react";
 
 function App() {
-  //we are making usestate here
+//we are making usestate here
 
-  const[weight, setWeight] = useState(0)
-  const[height, setHeight] = useState(0)
-  const[bmi, setBmi] = useState('')
-  const[message, setMessage] = useState('')
+const[weight , setWeight] = useState(0)
+const[height , setHeight] = useState(0)
+const[bmi , setBmi] = useState('')
+const[message , setMessage] = useState('')
 
-  let calcBmi = (event) => {
-    //preventing submitting to the server
-    event.preventDefault()
+//logic part
 
-  //making bmi calculator formula
+let calcBmi = (event) => {
+  event.preventDefault()
+
   if (weight === 0 || height === 0) {
     alert('Please enter a valid weight and height')
-  } 
-  else {
-    let calculatedBmi = (weight / (height * height) * 703)
-    setBmi (bmi.toFixed(1))
-  
-
-  //logic for message
-
-  if(calculatedBmi < 18.5){
-    setMessage("You are underweight")
-  } else if (calculatedBmi >= 18.5 && calculatedBmi < 25) {
-    setMessage("You are a healthy person")
   } else {
-    setMessage("You are overweight")
+    let calculatedBmi = (weight / (height * height) * 703)
+    setBmi(calculatedBmi.toFixed(1))
+
+    if (calculatedBmi < 25) {
+      setMessage("You are underweight")
+    } else if (calculatedBmi >= 25 && calculatedBmi < 30) {
+      setMessage("You are a healthy Person")
+    } else {
+      setMessage("You are OverWeight")
+    }
   }
-
 }
-}
-
 let reload = () => {
   window.location.reload()
 }
+
+
+
   return (
     <div className="app">
       <div className="container">
         <h2 className="center">BMI Calculator</h2>
         <form on onSubmit={calcBmi}>
-          
           <div>
             <label> Weight (lbs)</label>
             <input
@@ -54,8 +50,8 @@ let reload = () => {
               onChange={(e) => setWeight(e.target.value)}
             />
           </div>
-          <div>
-            <label> Height (inch)</label>
+           <div>
+            <label> Height (in)</label>
             <input
               type="text"
               placeholder="Enter Height value"
@@ -64,17 +60,17 @@ let reload = () => {
             />
           </div>
           <div>
-            <button className= 'btn' type= 'submit'>Submit</button>
-            <button className= 'btn btn-outline' onClick={reload} type='button'>Reload</button>
+            <button className='btn' type= 'submit'>Submit</button>
+            <button className='btn btn-outline' onClick={reload} type='button'>Reload</button>
           </div>
         </form>
         <div className="center">
           <h3> Your BMI is : {bmi}</h3>
           <p>{message}</p>
-        </div>
       </div>
     </div>
+    </div>
   );
-
 }
+
 export default App;
